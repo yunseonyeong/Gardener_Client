390a42a97134f1e65e50ed6dbb83608b001ee3b5
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-03-27 01:16:02 +0900

    delete bluenav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Link, Route, Switch } from 'react-router-dom';
-import Basicnav from "./components/nav/basicNav/basicNav";
-import Bluenav from "./components/nav/blueNav/blueNav";
+import SearchPage from "./pages/searchPage/searchPage";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/">
-        <Basicnav></Basicnav>
-      </Route>
-      <Route path="/blue">
-        <Bluenav></Bluenav>
+      <Route exact path="/">
+        <SearchPage />
       </Route>
     </div>
   );
---
+++ src/pages/searchPage/searchPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import BasicNav from '../../components/nav/basicNav/basicNav.js'
+
+const searchPage = () => {
+  return (
+    <div className='searchPage'>
+      <BasicNav>
+      </BasicNav>
+      <div style={{height: "500px" }}>
+        fwefweew
+      </div>
+    </div>
+  )
+}
+
+export default searchPage
