50b2959bc788339258acd741590228c8153399b9
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-03-27 19:07:24 +0900

    search bar icon

--- src/components/search/searchBar/searchBar.js
+++ src/components/search/searchBar/searchBar.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import getImgUrl from '../../../globalLogic';
+import { BiSearchAlt2 } from "react-icons/bi";
 
 const SearchBarDom = styled.div`
   background-color: #E3F2B8;
-  width:50%;
+  width:55%;
   border-radius : 50px;
   display: flex;
-  justify-content: center;
   align-items: center;
 `;
 
 const SearchBarInput = styled.input`
-  width: 90%;
+  width: 75%;
   background-color: #e3f2b8;
   border: none;
   padding-top:3%;
@@ -22,16 +21,18 @@ const SearchBarInput = styled.input`
    }
 `;
 const SearchBarIcon = styled.div`
-   width:5%;
+  padding-left: 5%;
+  padding-right: 3%;
+  
 `
 
 function searchBar() {
   return (
     <SearchBarDom>
-      <SearchBarInput />
       <SearchBarIcon>
-        <img src={getImgUrl("searchIcon")} />
+        <BiSearchAlt2 />
       </SearchBarIcon>
+      <SearchBarInput />
     </SearchBarDom>
   );
 }
