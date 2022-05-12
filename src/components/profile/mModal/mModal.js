749b4edab0a70c47a8657717347c507e05b277b2
tngur1101 <tngur1101@naver.com>
2022-05-12 21:19:40 +0900

    add profilepage

---
+++ src/components/profile/mModal/mModal.js
@@ -0,0 +1,71 @@
+import React,{useState} from "react";
+import styled from "styled-components";
+import { AiOutlineMail,AiOutlineSend } from "react-icons/ai";
+import Modal from "react-modal";
+
+Modal.setAppElement('#root');
+
+const MessageModal = (props) => {
+    const [modalIsOpen, setModalIsOpen] = useState(0);
+
+    function toggleMdodal(){
+        setModalIsOpen(!modalIsOpen);
+    }
+
+    const MessageWrapper = styled.div`
+    height: 15%;
+    width: 15%;
+    display: inline-block;
+    margin-top: 0.8rem;
+    margin-left: 31rem;
+    `;
+
+    const MessageDom = styled.span`
+    cursor: grab;
+    height: 20px;
+    width: 20px;
+    `;
+
+    const SendDom = styled.span`
+    cursor: grab;
+    height: 15%;
+    width: 15%;
+    `;
+
+    const ModalDom = styled.div`
+    width: 100px;
+    height: 200px;
+    display: inline-box;
+    `;
+
+    return (
+        <MessageWrapper>
+        <MessageDom onClick={toggleMdodal}><AiOutlineMail size={25}/></MessageDom>
+        
+        <Modal 
+        className="messageModal" 
+        isOpen = {modalIsOpen}
+        onRequestClose = {toggleMdodal}
+        contentLabel = "Message"
+        style={
+            {
+                overlay: {
+                    backgroundColor: 'grey'
+                },
+                content: {
+                    color: 'white'
+                }
+            }
+        }
+        >
+            <ModalDom>
+                안녕
+                <span onClick={toggleMdodal}><AiOutlineSend /></span>
+            </ModalDom>
+        </Modal>
+        </MessageWrapper>
+    );
+
+};
+
+export default MessageModal;
---
+++ src/pages/profilePage/profilePage.js
@@ -0,0 +1,208 @@
+import React, { useEffect, useState } from "react";
+import BlueNav from "../../components/nav/basicNav/blueNav";
+import styled from "styled-components";
+import getImgUrl from '../../globalLogic';
+import ExpBar from "../../components/profile/expBar/expBar";
+import { GitLawn } from "git-lawn";
+import { AiOutlineMessage } from "react-icons/ai";
+import MessageModal from "../../components/profile/mModal/mModal";
+import ChallengeState from "../../components/profile/challengebox/challengeList";
+import ChallengeContents from "../../components/profile/challengebox/challengeContents";
+import profilePageData from "../../data/profilePageData";
+import BasicNavTop from "../../components/nav/basicNav/basicNavTop";
+import {useParams} from 'react-router-dom';
+
+const ProfileWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    overflow-y:scroll;
+    height: 90vh;
+`;
+
+const ExpBarWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3%;
+    margin-left: 15%;
+`;
+
+const LogoDom = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 20%;
+  margin-left: 20%;
+`;
+
+const Logo = styled.img`
+  max-width: 90%;
+  margin-top : 7%;
+`;
+
+const ProfileDom = styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 80%;
+`;
+
+const GardenWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3%;
+    width: 80%;
+`;
+
+const ChallengeListWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    alignt-items: center;
+    flex-direction: column;
+    margin-bottom: 10%;
+`;
+
+const UserName = styled.span`
+    font-family: 'dungeunmo';
+    font-weight: 400;
+    font-size: 1.4rem;
+    margin-bottom: 3%;
+    margin-right: 2%;
+    margin-left : 1%;
+`;
+
+const MbtiName = styled.span`
+    font-family: 'dungeunmo';
+    font-weight: 400;
+    font-size: 0.9rem;
+    margin-left: 2%;
+    margin-right: 1%;
+`;
+
+const ChallengeTitleDom = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5%;
+    margin-left: 10%;
+`;
+
+const ChallengeBodyDom = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: 2%;
+    margin-left: 5%;
+`;
+
+const GrassFooter = styled.img`
+  width: 100%;
+  bottom: 0;
+  position: absolute;
+  height: 10%;
+  overflow: hidden;
+`;
+
+const Profilewrap = styled.div`
+    display: flex;
+    margin-bottom: 0.7rem;
+    width: 90%;
+`;
+
+const TierImg = styled.img`
+width: 2rem;
+`;
+
+const TierDom = styled.span`
+    margin-left : 2%;
+    margin-right: 1%;
+`;
+
+const contentbody = styled.div`
+    display: inline-block;
+    height: 40%;
+    width: 30%;
+`;
+
+const NameDom = styled.div`
+    display:flex;
+    flex-direction : column;
+`;
+const GITHUB_USERNAME = "tngur1101";
+
+// const testData = [
+//     {bgcolor: "#7FC087", getexp: 100 },
+// ];
+
+
+const ProfilePage = () => {
+    const [profileData, setProfileData] = useState([]);
+    const [listData, setListData] = useState([]);
+    const {profileId}=useParams();
+
+    const fetchData = ()=>{
+        const data = profilePageData;
+        return data;
+    }
+
+    useEffect(() => {
+        const data = fetchData();
+        console.log(data);
+        setProfileData(data.profile);
+        setListData(data.challenges);
+    
+      }, [])
+
+      useEffect(() => {
+        console.log(profileData)
+      }, [profileData])
+
+    return(
+        <ProfileWrapper>
+            <BasicNavTop />
+
+            <ExpBarWrapper>
+                <ProfileDom>
+                    {/* <UserName>{profileData.nickname}</UserName> */}
+                    <Profilewrap>
+                    <NameDom>
+                    <MbtiName>폭신한 라일락</MbtiName>
+                    <UserName>yunsyonng</UserName>
+                    </NameDom>
+                    <TierDom>
+                    <TierImg src={getImgUrl("flower")}/>
+                    </TierDom>
+                    <MessageModal />
+                    </Profilewrap>
+                        <ExpBar getexp = "60" />
+                        
+                    {/* <MbtiName>{profileData.devType}</MbtiName> */}
+                    
+                    
+                </ProfileDom>
+                
+            </ExpBarWrapper>
+
+            <GardenWrapper>
+            <LogoDom>
+                    <Logo src={getImgUrl("boyLogo")} />
+            </LogoDom>
+                <GitLawn username = {GITHUB_USERNAME} month={4} grassSpan={15} grassShape={"Circle"}/>
+            </GardenWrapper>
+
+            <ChallengeListWrapper>
+                <ChallengeTitleDom>
+                    <ChallengeState />
+                </ChallengeTitleDom>
+                <ChallengeBodyDom>
+                    <ChallengeContents listData = {listData}/>
+                </ChallengeBodyDom>
+            </ChallengeListWrapper>
+            <GrassFooter src={getImgUrl("basicgrass")} />
+        </ProfileWrapper>
+    );
+};
+
+export default ProfilePage;
