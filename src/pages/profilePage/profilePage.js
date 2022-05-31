b1aa8abbf7d30a7e83cf01be83dcf1cc67c25aaa
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-06-01 06:04:46 +0900

    tier type lawn color

--- src/pages/profilePage/profilePage.js
+++ src/pages/profilePage/profilePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import BlueNav from "../../components/nav/basicNav/blueNav";
 import styled from "styled-components";
-import getImgUrl from '../../globalLogic';
+import getImgUrl from "../../globalLogic";
 import ExpBar from "../../components/profile/expBar/expBar";
 import { GitLawn } from "git-lawn-hackathon";
 import MessageModal from "../../components/profile/mModal/mModal";
@@ -9,24 +9,24 @@ import ChallengeState from "../../components/profile/challengebox/challengeList"
 import ChallengeContents from "../../components/profile/challengebox/challengeContents";
 import profilePageData from "../../data/profilePageData";
 import BasicNavTop from "../../components/nav/basicNav/basicNavTop";
-import {useParams, Link} from 'react-router-dom';
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import { TiHeart } from "react-icons/ti";
 import LevelUpModal from "../../components/profile/mModal/levelUpModal";
 
 const ProfileWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    overflow-y:scroll;
-    height: 90vh;
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+  height: 90vh;
 `;
 
 const ExpBarWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 3%;
-    margin-left: 15%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3%;
+  margin-left: 15%;
 `;
 
 const LogoDom = styled.div`
@@ -38,62 +38,64 @@ const LogoDom = styled.div`
 
 const Logo = styled.img`
   max-width: 90%;
-  margin-top : 7%;
+  margin-top: 7%;
 `;
 
 const ProfileDom = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 80%;
 `;
 
 const GardenWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 3%;
-    width: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3%;
+  width: 80%;
 `;
 
 const ChallengeListWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    margin-bottom: 10%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 10%;
 `;
 
 const UserName = styled.span`
-    font-family: 'dungeunmo';
-    font-weight: 400;
-    font-size: 1.4rem;
-    margin-bottom: 3%;
-    margin-right: 2%;
-    margin-left : 1%;
+  font-family: "dungeunmo";
+  font-weight: 400;
+  font-size: 1.4rem;
+  margin-bottom: 3%;
+  margin-right: 2%;
+  margin-left: 1%;
 `;
 
 const MbtiName = styled.span`
-    font-family: 'dungeunmo';
-    font-weight: 400;
-    font-size: 0.9rem;
-    margin-left: 2%;
-    margin-right: 1%;
-    width: 100%;
+  font-family: "dungeunmo";
+  font-weight: 400;
+  font-size: 0.9rem;
+  margin-left: 2%;
+  margin-right: 1%;
 `;
 
 const ChallengeTitleDom = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    margin-top: 3%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5%;
+  margin-left: 10%;
 `;
 
 const ChallengeBodyDom = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    margin-top : 2%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2%;
+  margin-left: 5%;
 `;
 
 const GrassFooter = styled.img`
@@ -105,34 +107,35 @@ const GrassFooter = styled.img`
 `;
 
 const Profilewrap = styled.div`
-    display: flex;
-    margin-bottom: 0.7rem;
-    width: 90%;
-
-    .Heart{
-        margin-left:65%;
-        cursor: pointer;
-    }
+  display: flex;
+  margin-bottom: 0.7rem;
+  width: 90%;
+  .Heart {
+    margin-left: 35%;
+    cursor: pointer;
+    width: 20%;
+  }
 `;
 
 const TierImg = styled.img`
-width: 2rem;
+  width: 15%;
 `;
 
-const TierDom = styled.span`
-    margin-left : 2%;
-    margin-right: 1%;
+const TierDom = styled.div`
+  margin-left: 2%;
+  width: 30%;
+  height: 10%;
 `;
 
 const contentbody = styled.div`
-    display: inline-block;
-    height: 40%;
-    width: 30%;
+  display: inline-block;
+  height: 40%;
+  width: 30%;
 `;
 
 const NameDom = styled.div`
-    display:flex;
-    flex-direction : column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const LevelUpDom = styled.div`
@@ -147,9 +150,8 @@ const LevelUpDom = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 5px dashed #13A74D;
+  border: 5px dashed #13a74d;
   box-sizing: content-box;
-  font-family : 'dungeunmo';
 `;
 
 const GITHUB_USERNAME = "leobang17";
@@ -159,114 +161,225 @@ const GITHUB_USERNAME = "leobang17";
 // ];
 
 const handleTierLawn = (tier) => {
-  if(tier === 'bronze'){
-    return 'GREEN';
+  if (tier === "bronze") {
+    return "GREEN";
+  } else if (tier === "silver") {
+    return "ORANGE";
+  } else if (tier === "pink") {
+    return "PINK";
+  } else if (tier === "purple") {
+    return "PURPLE";
+  } else if (tier === "platinum") {
+    return "BLUE";
   }
-  else if(tier === 'silver'){
-    return 'ORANGE';
-  }
-  else if(tier === 'pink'){
-    return 'PINK';
-  }
-  else if(tier === 'purple'){
-    return 'PURPLE';
-  }
-  else if(tier === 'platinum'){
-    return 'BLUE';
-  }
-}
+};
 
+const ChalDom = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 2;
+`;
+
+const ListTitle = styled.div`
+  display: flex;
+  margin-bottom: 3%;
+  font-family: "dungeunmo";
+`;
+
+const ListImg = styled.img`
+  max-width: 60%;
+`;
 
 const ProfilePage = () => {
-    const [profileData, setProfileData] = useState([]);
-    const [listData, setListData] = useState([]);
-    const {id}=useParams();
-    const [messageModalOpen, setMessageModalOpen]= useState(false);
-    const [levelUp, setLevelUp]=useState(false);
+  const [profileData, setProfileData] = useState([]);
+  const [listData, setListData] = useState([]);
+  const { profileId } = useParams();
+  const [messageModalOpen, setMessageModalOpen] = useState(false);
+  const [levelUp, setLevelUp] = useState(false);
+  const [bgColor, setBgColor] = useState([]);
 
-    const handleModal = () => {
-        setMessageModalOpen(!messageModalOpen); 
+  const handleModal = () => {
+    setMessageModalOpen(!messageModalOpen);
+  };
+
+  const fetchData = async () => {
+    // const data = await axios.get(`http://localhost:8000/api/user/${profileId}`)
+    // setProfileData(data.data.profile);
+    // setListData(data.data.challenges);
+    // console.log(data.data.profile);
+    // console.log(profileData);
+    const data = profilePageData;
+    console.log(data);
+    setProfileData(data.profile);
+    setListData(data.challenges);
+    console.log(profileData);
+    console.log(listData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (profileData.levelup === true) {
+      setLevelUp(true);
     }
+  }, [profileData]);
 
-    const fetchData = async ()=>{
-        /*
-        const data = await axios.get(`http://localhost:8000/api/user/${id}`)
-        setProfileData(data.data.profile);
-        setListData(data.data.challenges);
-        console.log(data.data.profile);
-        console.log(data.data.challenges);
-        console.log('----------------------------------------------------');
-        console.log(listData);
-        */
-         const data=profilePageData;
-         setProfileData(data.profile);
-         setListData(data.challenges);
-         console.log(profileData);
+  const SelectTierImg = (tier, tierNum) => {
+    if (tier === "bronze") {
+      if (tierNum === 1) {
+        return <TierImg src={getImgUrl("bronze1")} />;
+      } else if (tierNum === 2) {
+        return <TierImg src={getImgUrl("bronze2")} />;
+      } else if (tierNum === 3) {
+        return <TierImg src={getImgUrl("bronze3")} />;
+      } else if (tierNum === 4) {
+        return <TierImg src={getImgUrl("bronze4")} />;
+      } else if (tierNum === 5) {
+        return <TierImg src={getImgUrl("bronze5")} />;
+      }
+    } else if (tier === "silver") {
+      if (tierNum === 1) {
+        return <TierImg src={getImgUrl("silver1")} />;
+      } else if (tierNum === 2) {
+        return <TierImg src={getImgUrl("silver2")} />;
+      } else if (tierNum === 3) {
+        return <TierImg src={getImgUrl("silver3")} />;
+      } else if (tierNum === 4) {
+        return <TierImg src={getImgUrl("silver4")} />;
+      } else if (tierNum === 5) {
+        return <TierImg src={getImgUrl("silver5")} />;
+      }
+    } else if (tier === "pink") {
+      if (tierNum === 1) {
+        return <TierImg src={getImgUrl("pink1")} />;
+      } else if (tierNum === 2) {
+        console.log(tierNum);
+        return <TierImg src={getImgUrl("pink2")} />;
+      } else if (tierNum === 3) {
+        return <TierImg src={getImgUrl("pink3")} />;
+      } else if (tierNum === 4) {
+        return <TierImg src={getImgUrl("pink4")} />;
+      } else if (tierNum === 5) {
+        return <TierImg src={getImgUrl("pink5")} />;
+      }
+    } else if (tier === "purple") {
+      if (tierNum === 1) {
+        return <TierImg src={getImgUrl("purple1")} />;
+      } else if (tierNum === 2) {
+        return <TierImg src={getImgUrl("purple2")} />;
+      } else if (tierNum === 3) {
+        return <TierImg src={getImgUrl("purple3")} />;
+      } else if (tierNum === 4) {
+        return <TierImg src={getImgUrl("purple4")} />;
+      } else if (tierNum === 5) {
+        return <TierImg src={getImgUrl("purple5")} />;
+      }
+    } else if (tier === "platinum") {
+      if (tierNum === 1) {
+        return <TierImg src={getImgUrl("platinum1")} />;
+      } else if (tierNum === 2) {
+        return <TierImg src={getImgUrl("platinum2")} />;
+      } else if (tierNum === 3) {
+        return <TierImg src={getImgUrl("platinum3")} />;
+      } else if (tierNum === 4) {
+        return <TierImg src={getImgUrl("platinum4")} />;
+      } else if (tierNum === 5) {
+        return <TierImg src={getImgUrl("platinum5")} />;
+      }
     }
+  };
 
-    useEffect(() => {
-        fetchData();
-      },[])
+  const SelectLogoImg = (tier) => {
+    if (tier === "bronze") {
+      return <Logo src={getImgUrl("boyLogo")} />;
+    } else if (tier === "silver") {
+      return <Logo src={getImgUrl("boyLogo2")} />;
+    } else if (tier === "pink") {
+      return <Logo src={getImgUrl("boyLogo3")} />;
+    } else if (tier === "purple") {
+      return <Logo src={getImgUrl("boyLogo4")} />;
+    } else if (tier === "platinum") {
+      return "BLUE";
+    }
+  };
 
-      useEffect(() => {
-        if(profileData.levelup===true){
-            setLevelUp(true);
-        }
-      }, [profileData])
+  useEffect(() => {
+    if (profileData.tierType === "bronze") {
+      setBgColor(["#5fb871", "#8ed49c"]);
+    } else if (profileData.tierType === "pink") {
+      setBgColor(["#e88b5d", "#fae6dc"]);
+    } else if (profileData.tierType === "purple") {
+      setBgColor(["#723491", "#a882cf"]);
+    }
+  }, [profileData]);
 
-    return(
-        <ProfileWrapper>
-            <BasicNavTop />
+  return (
+    <ProfileWrapper>
+      <BasicNavTop />
 
-            <ExpBarWrapper>
-                <ProfileDom>
-                    {/* <UserName>{profileData.nickname}</UserName> */}
-                    <Profilewrap>
-                    <NameDom>
-                    <MbtiName>{profileData.devType}</MbtiName>
-                    <UserName>{profileData.nickname}</UserName>
-                    </NameDom>
-                    <TierDom>
-                    <TierImg src={getImgUrl("flower")}/>
-                    </TierDom>
-                    <TiHeart color="coral" size={35} className="Heart" onClick={handleModal}/>
-                    {messageModalOpen&&<MessageModal setMessageModalOpen={setMessageModalOpen}/>}
-                    </Profilewrap>
-                        <ExpBar tierType = {profileData.tierType} getexp = {profileData.exp} />
-                        
-                    {/* <MbtiName>{profileData.devType}</MbtiName> */}
-                    
-                    
-                </ProfileDom>
-                
-            </ExpBarWrapper>
+      <ExpBarWrapper>
+        <ProfileDom>
+          {/* <UserName>{profileData.nickname}</UserName> */}
+          <Profilewrap>
+            <NameDom>
+              <MbtiName>{profileData.devType}</MbtiName>
+              <UserName>{profileData.nickname}</UserName>
+            </NameDom>
+            <TierDom>
+              {SelectTierImg(profileData.tierType, profileData.tierNum)}
+            </TierDom>
+            <TiHeart
+              color="coral"
+              size={35}
+              className="Heart"
+              onClick={handleModal}
+            />
+            {messageModalOpen && (
+              <MessageModal setMessageModalOpen={setMessageModalOpen} />
+            )}
+          </Profilewrap>
 
-            <GardenWrapper>
-            <LogoDom>
-                    <Logo src={getImgUrl("boyLogo")} />
-            </LogoDom>
-                <GitLawn username = {GITHUB_USERNAME} month={4} grassSpan={20} grassShape={"Circle"} color={handleTierLawn(profileData.tierType)}/>
-            </GardenWrapper>
+          <ExpBar bgcolor={bgColor} getexp={profileData.exp} />
+        </ProfileDom>
+      </ExpBarWrapper>
 
-            <ChallengeListWrapper>
-                <ChallengeTitleDom>
-                    <ChallengeState />
-                </ChallengeTitleDom>
-                <ChallengeBodyDom>
-                    <ChallengeContents listData = {listData}/>
-                </ChallengeBodyDom>
-            </ChallengeListWrapper>
-            <GrassFooter src={getImgUrl("basicgrass")} />
-            {
-                levelUp ? (
-                    <LevelUpDom>
-                        <LevelUpModal setLevelUp={setLevelUp}/>
-                    </LevelUpDom>
-                ):null
-            }
-        </ProfileWrapper>
-        
-    );
+      <GardenWrapper>
+        <LogoDom>{SelectLogoImg(profileData.tierType)}</LogoDom>
+        <GitLawn
+          username={GITHUB_USERNAME}
+          month={4}
+          grassSpan={20}
+          grassShape={"Circle"}
+          color={handleTierLawn(profileData.tierType)}
+        />
+      </GardenWrapper>
+
+      <ChallengeListWrapper>
+        <ChallengeTitleDom>
+          <ChallengeState />
+        </ChallengeTitleDom>
+        <ChallengeBodyDom>
+          <ChallengeContents listData={listData} />
+          {/* <Link to={`/challenge/${listData.challengeId}`}>
+            <ChalDom>
+              <ListTitle>{listData.challengeTitle}</ListTitle>
+              <ListImg src={listData.challengeImgURL} />
+            </ChalDom>
+          </Link> */}
+        </ChallengeBodyDom>
+      </ChallengeListWrapper>
+      <GrassFooter src={getImgUrl("basicgrass")} />
+      {levelUp ? (
+        <LevelUpDom>
+          <LevelUpModal setLevelUp={setLevelUp} />
+        </LevelUpDom>
+      ) : null}
+    </ProfileWrapper>
+  );
 };
 
 export default ProfilePage;
