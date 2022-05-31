94ef0dde591a9b2a27cfea746b1a18c1534ca5d2
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-06-01 06:03:41 +0900

    exp gradient

--- src/components/profile/expBar/expBar.js
+++ src/components/profile/expBar/expBar.js
@@ -15,14 +15,14 @@ const expBar = (props) => {
     const FilterStyle = styled.div`
         height: 100%;
         width: ${getexp}%;
-        background: linear-gradient(90deg, #FF7056 0%, #FFD6CF 100%);
+        background: linear-gradient(90deg, ${bgcolor[0]} 0%, ${bgcolor[1]} 100%);
         border-radius: 10px;
         text-align: 'right';
     `;
 
     const labelStyles = {
         padding: 5,
-        color: 'gray',
+        color: 'white',
         fontWeight: 'bold',
         fontSize: 5,
         float: "right",
